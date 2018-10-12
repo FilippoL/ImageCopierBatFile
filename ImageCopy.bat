1eaf847fbec721e07a41990fe60b0750d714b244
Unknown <pippo.libardi@gmail.com>
2018-10-12 17:52:01 +0100

    Updated version
    Applied extra features as requested from client

--- ImageCopy.bat
+++ ImageCopy.bat
@@ -1,4 +1,5 @@
 @echo off
+
 :while
 set /P _source_folder=Please enter source folder:
 if exist %_source_folder% (
@@ -15,22 +16,22 @@ if exist %_destination_folder% (
   echo NO..
   goto :while2
 )
-set /P _image_format=Please enter image format you'd like to copy (leave empty for all common formats -.jpeg, .png, .jpg- ):
-if [%_image_format] == [] set _image_format=".*"
-set /P _image_name=Please enter a single file name (leave empty for all images):
-if [%_image_name] == [] set _image_name="*"
-set _fullpath=%_source_folder%\*%_image_name%%_image_format%
-xcopy /f /j /s /w /z %_fullpath% %_destination_folder%\
-set /P _ans=Do you want to complete another operation?(y/n)
-if %_ans% == Y (
-  cls
-  goto :while
-) else (
-  if %_ans% == y (
-  cls
-  goto :while
-) else (
-  echo All done...
-)
-)
+
+for /f "tokens=*" %%a in (input.txt) do call :processline %%a
+
+pause
+goto :eof
+
+:processline
+set _image_name="%*"
+
+set _fullpath=%_source_folder%\*%_image_name%
+xcopy /f /j /s /z %_fullpath% %_destination_folder%\
+
+goto :eof
+
+:eof
+
+set /P _ans=Do you want to close the window?(y/n)
+
 pause
